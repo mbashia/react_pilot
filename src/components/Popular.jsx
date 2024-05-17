02b3f9d592251a8bcbbb33dbc7f0c29ae600d37e
mbashia <mbashiavictor@gmail.com>
2024-05-17 18:22:28 +0300

    added

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,12 +1,36 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 function Popular() {
+  const [popular, setPopular] = useState([]);
+  useEffect(() => {
+    getPopular();
+  }, []);
   const getPopular = async () => {
     const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apikey=${process.env.REACT_APP_API_KEY}&number=9`
+      `https://api.spoonacular.com/recipes/random?apiKey=e87bfba47e4f48848b6990f5e4c48913`
     );
+
+    const data = await api.json();
+    console.log(data);
+    setPopular(data.recipes);
   };
-  return <div>Popular</div>;
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <Wrapper>
+            <h3>popular Picks</h3>
+            <div key={recipe.id}>
+              <p>{recipe.title}</p>
+            </div>
+          </Wrapper>
+        );
+      })}
+    </div>
+  );
 }
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
 
 export default Popular;
